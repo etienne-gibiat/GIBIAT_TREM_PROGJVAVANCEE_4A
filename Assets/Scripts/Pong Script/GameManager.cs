2c5eff0379dd850f05048090793243a18e625b6f
Guillaume <guillaume.trem@outlook.fr>
2020-09-29 14:58:43 +0200

    tranisition de pong à windjammers
    la balle ne percute pas

---
+++ Assets/Scripts/Pong Script/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject BallPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        Instantiate<GameObject>(BallPrefab, Vector3.zero, Quaternion.identity);
+    }
+}
+
